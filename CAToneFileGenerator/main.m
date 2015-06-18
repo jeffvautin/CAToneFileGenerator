0e573a39b675eca1e29c41b923279126a66ccfe6
Jeff Vautin <jeff@jeffvautin.com>
2015-06-17 22:44:34 -0400

    Initial Commit

---
+++ CAToneFileGenerator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CAToneFileGenerator
+//
+//  Created by Jeff Vautin on 6/17/15.
+//  Copyright © 2015 Jeff Vautin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
